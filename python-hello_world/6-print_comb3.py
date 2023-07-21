8a65fcce386244973d1216ab5df0ef258a72fee9
Unkle-G <roimykel@gmail.com>
2023-07-21 05:48:24 -0700

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-combinations = []
 for y in range(10):
     for x in range(y+1, 10):
-        combinations.append("{:02d}".format(y * 10 + x))
-
-print(", ".join(combinations))
- 
+        print("{:02d}".format(y * 10 + x), end=', ' if x < 9 else '\n')
