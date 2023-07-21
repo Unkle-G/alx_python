6de2def113bd09d93f9bee19c2f54e5c9dbcc64b
Unkle-G <roimykel@gmail.com>
2023-07-21 06:23:17 -0700

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for y in range(10):
     for x in range(y + 1, 10):
-        print("{:02d}".format(y * 10 + x), end=', ' if y < 8 or x < 9 else ' ') 
+        print("{:02d}".format(y * 10 + x), end=', ' if x < 9 else ' ') 
